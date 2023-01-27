d1b55d8a0c370068695fca9656f1ef6b728e7f2a
spido-pankaj <115925843+spido-pankaj@users.noreply.github.com>
2023-01-27 18:11:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var myForm = document.getElementById("myForm");
+var myInput = document.getElementById("myInput");
+var myItem = document.getElementById("myItem");
+myForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  createItem(myInput.value);
+});
+function createItem(inputItems) {
+  var items = `<li>${inputItems}
+    <button onclick="deleteElement(this)">Delete</button> </li>`;
+  myItem.insertAdjacentHTML("beforeend", items);
+  myInput.value = "";
+  myInput.focus();
+}
+function deleteElement(ElementToDelete) {
+  ElementToDelete.parentElement.remove();
+}
